55b0464fe51ff285d9f0aad4ef11a37cf3befcaa
augusto.oliveira <augusto.oliveira@coopersystem.com.br>
2025-10-01 20:21:25 -0300

    chore: Novos selects adicionados

--- apiary/static/apiary/js/hive_species_filter.js
+++ apiary/static/apiary/js/hive_species_filter.js
@@ -4,14 +4,28 @@
   }
 
   function initializeSpeciesFilter() {
-    // Select do filtro de espécies na lista de colmeias
-    $('.list-filter-dropdown').find('select').select2();
+    setTimeout(function() {
+      // Select do filtro de espécies na lista de colmeias
+      $('.list-filter-dropdown').find('select').select2();
 
-    // Select do formulário de colmeia
-    $('#id_species').select2();
+      // Select do formulário de colmeia
+      var speciesSelect = $('#id_species[name="species"]');
+      if (speciesSelect.length) {
+        speciesSelect.select2();
+      }
 
-    // Select do formulário de revisões de colmeia
-    $('#id_hive').select2();
+      // Select do formulário de revisões de colmeia
+      $('#id_hive').select2();
+      
+      // Select de cidades
+      $('#id_city').select2();
+      
+      // Select do formulário de origem de colmeia
+      $('#id_origin_hive').select2();
+
+      // Select do formulário de caixa de colmeia
+      $('#id_box_model').select2();
+    }, 200);
   }
 
   if (document.readyState === 'loading') {
